713c8983436dd60a75095abe5df430e9868d20c5
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-11-18 15:28:11 +0200

    update

--- moduuli13/teht2.py
+++ moduuli13/teht2.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from flask import Flask
+from flask_cors import CORS
 
 yhteys = mysql.connector.connect(
     host="mysql.metropolia.fi",
@@ -14,13 +15,27 @@ def haeLentokentta(koodi):
     sql = f"SELECT * FROM airport WHERE ident=%s"
     kursori = yhteys.cursor(dictionary=True)
     kursori.execute(sql, (koodi,))
-    return kursori.fetchone()
+    tulos = kursori.fetchone()
+    print('Debug', tulos)
+    return tulos
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/airport/<icao>')
 def get_airport(icao):
-    return haeLentokentta(icao)
+    try:
+        lentokentta = haeLentokentta(icao)
+        if lentokentta is None:
+            return {"error": "Lentokenttää ei löydy"}, 404
+        return lentokentta
+    except mysql.connector.errors.ProgrammingError as err:
+        print(err)
+        return {"error": "räätälöity virheilmoitus"}, 500
+    except Exception as err:
+        print(err)
+        return {"error": "geneerinen virheilmoitus"}, 500
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
